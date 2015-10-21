e8527b500e01466f8a84d647e91a07488d90a029
Maqix <stincodisanto@gmail.com>
2015-10-21 19:05:41 +0200

    Aggiunta la possibilità di eliminare un Auto (SENZA CONTROLLI)

--- src/view/HomeAdminViewController.java
+++ src/view/HomeAdminViewController.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import application.Main;
 import javafx.collections.FXCollections;
@@ -8,7 +9,10 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
@@ -100,7 +104,54 @@ public class HomeAdminViewController
 	@FXML
 	private void eliminaAuto()
 	{
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("Elimina Auto");
+		alert.setHeaderText("Attenzione!");
+		alert.setContentText("Sei sicuro di voler eliminare quest'auto?");
+
+		//Ottengo l'auto selezionata
+		Auto autoSelezionata = tblAuto.getSelectionModel().getSelectedItem();
 		
+		//Se ho selezionato veramente una Auto
+		if (autoSelezionata != null)
+		{
+			//Se l'utente conferma di voler eliminare l'auto
+			Optional<ButtonType> result = alert.showAndWait();
+			if (result.get() == ButtonType.OK)
+			{
+				//Elimino l'auto dal DB
+				String comando = String.format("DELETE FROM `auto` WHERE `Targa` IN ('%s')", autoSelezionata.getTarga());
+				
+				//Se l'operazione sul DB va a buon fine
+				if (DAO.esegui(comando))
+				{
+					//Elimino l'auto dalla lista
+					listaAuto.remove(autoSelezionata);
+					//Avviso l'utente
+					Alert alert3 = new Alert(AlertType.INFORMATION);
+					alert3.setTitle("Elimina Auto");
+					alert3.setHeaderText("Auto eliminata");
+					alert3.setContentText(null);
+					alert3.showAndWait();
+				}else
+				{
+					//Avviso l'utente che l'operazione non è andata a buon fine
+					Alert alert4 = new Alert(AlertType.WARNING);
+					alert4.setTitle("Elimina Auto");
+					alert4.setHeaderText("Nessuna auto eliminata");
+					alert4.setContentText("C'è stato un problema col Database, contattare l'amministratore");
+					alert4.showAndWait();
+				}
+			}
+			
+		}else
+		{
+			Alert alert2 = new Alert(AlertType.WARNING);
+			alert2.setTitle("Elimina Auto");
+			alert2.setHeaderText("Nessuna auto selezionata");
+			alert2.setContentText("Seleziona una auto nell'elenco per eliminarla");
+			alert2.showAndWait();
+		}
 	}
 	
 	@FXML
