95bafe92e2a7ea49ddb63c09c9cd7b166fc009b0
Maqix <stincodisanto@gmail.com>
2015-10-24 16:20:18 +0200

    Aggiunto il selettore di province in aggiungi agenzia

--- src/view/AggiungiAgenziaViewController.java
+++ src/view/AggiungiAgenziaViewController.java
@@ -1,9 +1,11 @@
 package view;
 
 import application.Main;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import model.Agenzia;
@@ -19,8 +21,8 @@ private Stage dialogStage;
 	private TextField nomeTF;
 	@FXML
 	private TextField cittaTF;
-	@FXML
-	private TextField provinciaTF;
+	//@FXML
+	//private TextField provinciaTF;
 	@FXML
 	private TextField viaTF;
 	@FXML
@@ -29,9 +31,18 @@ private Stage dialogStage;
 	private Button aggiungiAgenziaButton;
 	@FXML
 	private Button annullaButton;
+	@FXML 
+	private ChoiceBox<String> provinceCB;
 	
 	private ObservableList<Agenzia> listaAgenzie;
 	
+	private ObservableList<String> province = FXCollections.observableArrayList();
+	
+	@FXML
+	private void initialize() 
+	{
+		configuraPicker();
+    }
 	
 	@FXML
 	private void aggiungiAgenzia()
@@ -42,7 +53,7 @@ private Stage dialogStage;
 			 String partitaIva= partitaIvaTF.getText();
 			 String nome= nomeTF.getText();
 			 String citta = cittaTF.getText();
-			 String provincia = provinciaTF.getText(); 
+			 String provincia = provinceCB.getSelectionModel().getSelectedItem(); 
 			 String via = viaTF.getText();
 			 String civico;
 			 if((civicoTF.getText().length())==0)
@@ -81,7 +92,16 @@ private Stage dialogStage;
 				}
 		}
 
-
+	private void configuraPicker()
+	{
+		String[] listaProvince = {"AG","AL","AN","AO","AR","AP","AT","AV","BA","BT","BL","BN","BG","BI","BO","BZ","BS","BR","CA","CL","CB","CE","CT","CZ","CH","CO","CS","CR","KR","CN","EN","FM","FE","FI","FG","FC","FR","GE","GO","GR","IM","IS","AQ","SP","LT","LE","LC","LI","LO","LU","MC","MN","MS","MT","ME","MI","MO","MB","NA","NO","NU","OR","PD","PA","PR","PV","PG","PU","PE","PC","PI","PT","PN","PZ","PO","RG","RA","RC","RE","RI","RN","RM","RO","SA","SS","SV","SI","SR","SO","TA","TE","TR","TO","TP","TN","TV","TS","UD","VA","VE","VB","VC","VR","VV","VI","VT"};
+		for (String prov: listaProvince)
+		{
+			province.add(prov);
+		}
+		provinceCB.setItems(province);	
+		provinceCB.getSelectionModel().selectFirst();
+	}
 	
 	@FXML
 	private void premutAnnulla()
@@ -89,16 +109,16 @@ private Stage dialogStage;
 		 dialogStage.close();
 	}
 	
-	  public void setDialogStage(Stage dialogStage) 
+	public void setDialogStage(Stage dialogStage) 
 	    {
 	        this.dialogStage = dialogStage;
 	    }
 
-		public ObservableList<Agenzia> getListaAgenzia() {
+	public ObservableList<Agenzia> getListaAgenzia() {
 			return listaAgenzie;
 		}
 
-		public void setListaAgenzie(ObservableList<Agenzia> listaAgenzie) {
+	public void setListaAgenzie(ObservableList<Agenzia> listaAgenzie) {
 			this.listaAgenzie = listaAgenzie;
 		}
 }
