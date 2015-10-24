eb7a2a008810e8f982a295efeb9f3f135e8951ee
Maqix <stincodisanto@gmail.com>
2015-10-24 20:55:01 +0200

    Configurato il Picker agenzie in AggiungiDipendente

--- src/view/AggiungiDipendenteViewController.java
+++ src/view/AggiungiDipendenteViewController.java
@@ -1,5 +1,8 @@
 package view;
 
+import java.util.ArrayList;
+
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -7,11 +10,11 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import model.DAO;
 import model.Dipendente;
 
 public class AggiungiDipendenteViewController 
 {
-	@SuppressWarnings("unused")
 	private Stage dialogStage;
 	
 	private ObservableList<Dipendente> listaDipendenti;
@@ -35,6 +38,8 @@ public class AggiungiDipendenteViewController
 	@FXML
 	private Button annullaButton;
 	
+	private ObservableList<String> agenzie = FXCollections.observableArrayList();
+	
 	@FXML
 	private void premutoAggiungi()
 	{
@@ -44,14 +49,25 @@ public class AggiungiDipendenteViewController
 	@FXML
 	private void premutoAnnulla()
 	{
-		
+		dialogStage.close();
 	}
 	
 	
 	@FXML
 	public void initialize()
 	{
-		
+		configuraPicker();
+	}
+	
+	private void configuraPicker()
+	{
+		ArrayList<String> listaAgenziePresenti = DAO.getListaString("agenzia", "Nome");
+		for (String agenzia: listaAgenziePresenti)
+		{
+			agenzie.add(agenzia);
+		}
+		agenziaCB.setItems(agenzie);
+		agenziaCB.getSelectionModel().selectFirst();
 	}
 	
     public void setDialogStage(Stage dialogStage) 
