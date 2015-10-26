dd6dcb7bf778593a7789c76b79008f78196eb68f
Maqix <stincodisanto@gmail.com>
2015-10-26 17:50:40 +0100

    Agganciati tutti i controlli al controller in ViewApriContratto

--- src/view/ApriContrattoViewController.java
+++ src/view/ApriContrattoViewController.java
@@ -1,6 +1,9 @@
 package view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class ApriContrattoViewController 
@@ -8,7 +11,48 @@ public class ApriContrattoViewController
 	private Stage dialogStage;
 	
 	@FXML
+	private ChoiceBox fasciaCB;
+	@FXML
+	private ChoiceBox noleggioCB;
+	@FXML
+	private ChoiceBox tariffaCB;
+	@FXML
+	private ChoiceBox rilascioCB;
+	@FXML
+	private ChoiceBox ritornoCB;
+	@FXML
+	private DatePicker dataInizioDF;
+	@FXML
+	private ChoiceBox clienteCB;
+	@FXML
+	private TextField nomeTF;
+	@FXML
+	private TextField cognomeTF;
+	@FXML
+	private TextField telefonoTF;
+	@FXML
+	private TextField codiceFiscaleTF;
+	@FXML
+	private TextField autoTF;
+	@FXML
+	private TextField accontoTF;
+	@FXML
+	private TextField contrattoTF;
+	
+	@FXML
 	private void initialize()
+	{
+		
+	}
+	
+	@FXML
+	private void premutoGeneraContratto()
+	{
+		
+	}
+	
+	@FXML
+	private void premutoApriContratto()
 	{
 		
 	}
